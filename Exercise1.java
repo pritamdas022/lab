7456e3df950ff1b6861c4f07146bf5cb3863fcb0
pritamdas022 <110466018+pritamdas022@users.noreply.github.com>
2022-08-12 18:14:26 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,30 @@
+// Q.1 write a java program that takes five numbers as input to calculate and print the average pf the numbers.
+import java.util.Scanner;
+ public class Exercise1 {
+ // main method
+ public static void main(String[] args) {
+ // input Scanner
+  Scanner in = new Scanner(System.in);
+  
+  System.out.print("Input first number: ");
+  int num1 = in.nextInt(); 
+  
+  System.out.print("Input second number: ");
+  int num2 = in.nextInt();
+  
+  System.out.print("Input third number: ");
+  int num3 = in.nextInt();
+  
+  System.out.print("Input fourth number: ");
+  int num4 = in.nextInt();
+  
+  System.out.print("Enter fifth number: ");
+  int num5 = in.nextInt();
+  
+  // print and output the average number
+  
+   System.out.println("Average of five numbers is: " + 
+  (num1 + num2 + num3 + num4 + num5) / 5);
+ }
+}
+
