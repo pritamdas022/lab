6c4ed878d0e51979356428c75f9b367e260bd656
pritamdas022 <110466018+pritamdas022@users.noreply.github.com>
2022-10-28 18:12:29 +0530

    Add files via upload

---
+++ 28th_oct_LAB/Problem3.java
@@ -0,0 +1,33 @@
+/*write a method called addTwoPositive that takes two int parameters, and if either value is not positive, throw an ArithmeticException,
+ * passing the string "Non-positive integer sent".to the constructor of the exception.if the the values are both positive,
+ * then return the sum of them*/
+package LAB;
+import java.lang.*;
+import java.util.Scanner;
+
+// main class
+public class Problem3 {
+// main method
+	public static void main(String[] args) {
+//		scanner class
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number 1: ");
+//		taking input 1
+		int a = sc.nextInt();
+		System.out.println("Enter number 2: ");
+//		taking input 2
+		int b = sc.nextInt();
+		
+		System.out.println(addTwoPositive(a,b));
+
+	}
+//	method
+	public static int addTwoPositive(int a,int b) throws ArithmeticException{
+		if (a<0||b<0) {
+			throw new ArithmeticException("Non-positive ineger sent");// throw arithmetic exception
+		}else {
+			return a+b; // returning sum
+		}
+	}
+
+}
